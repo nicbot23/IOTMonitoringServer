c8dee89b5e9fbf0b3b225afa2bc63442284e8331
Nicolas Ibarra <n.ibarra@uniandes.edu.co>
2026-03-02 10:30:25 -0500

    commit 6

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "18.213.245.84"]#["localhost", "ip.maquina.visualizador"]
+ALLOWED_HOSTS = ["localhost", "*"]#["localhost", "ip.maquina.visualizador"]
 
 
 # Application definition
